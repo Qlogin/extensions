720594db23d63e0f5d137f51f3c3e94a06c9d507
Roman <Roman.Qlogin@gmail.com>
2015-11-18 20:16:38 +0300

    map-switch: simplify

--- map-switch/data/link-click.js
+++ map-switch/data/link-click.js
@@ -1,4 +1,11 @@
-// Get selection elements and setup callbacks
+var elems = document.getElementsByClassName('selection');
+for (var i = 0; i != elems.length; ++i) {
+   elems[i].addEventListener('mouseover', select);
+   elems[i].addEventListener('mouseout', unselect);
+   elems[i].addEventListener('click', open_link);
+}
+
+// Selection and clicking callbacks
 function select(event) {
    event.target.style.border = '2px solid #b0da2e';
 }
@@ -10,21 +17,11 @@ function open_link(event) {
    self.port.emit('link-clicked', event.target.getAttribute('data-url'));
 }
 
-var elems = document.getElementsByClassName('selection');
-var i;
-for (i = 0; i != elems.length; ++i) {
-   elems[i].addEventListener('mouseover', select);
-   elems[i].addEventListener('mouseout', unselect);
-   elems[i].addEventListener('click', open_link);
-}
-
 // Listen for the "show" event being sent from the
 // main add-on code. It means that the panel's about
 // to be shown.
 self.port.on('show', function onShow(url) {
-   var elems = document.getElementsByClassName('selection');
-   var i;
-   for (i = 0; i != elems.length; ++i) {
+   for (var i = 0; i != elems.length; ++i) {
       if (url.contains(elems[i].getAttribute('data-url'))) {
          elems[i].children[0].style.display = 'inline';
       } else {
