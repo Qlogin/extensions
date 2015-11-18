77cb08593381c2affd9278ee1cfc6aafcee21f1a
Roman Kulagin <Roman.Kulagin@transas.com>
2015-11-18 17:24:36 +0300

    + don't make functions inside loop + set correct URLs

--- map-switch/data/link-click.js
+++ map-switch/data/link-click.js
@@ -1,16 +1,20 @@
 // Get selection elements and setup callbacks
+function select(event) {
+   event.target.style.border = '2px solid blue';
+}
+function unselect(event) {
+   event.target.style.border = '2px solid white';
+}
+function open_link(event) {
+   self.port.emit('link-clicked', event.target.getAttribute('data-url'));
+}
+
 var elems = document.getElementsByClassName('selection');
 var i;
 for (i = 0; i != elems.length; ++i) {
-   elems[i].addEventListener('mouseover', function(event) {
-      event.target.style.border = '2px solid blue';
-   });
-   elems[i].addEventListener('mouseout', function(event) {
-      event.target.style.border = '2px solid white';
-   });
-   elems[i].addEventListener('click', function(event) {
-      self.port.emit('link-clicked', 'https://www.yandex.ru');
-   });
+   elems[i].addEventListener('mouseover', select);
+   elems[i].addEventListener('mouseout', unselect);
+   elems[i].addEventListener('click', open_link);
 }
 
 // Listen for the "show" event being sent from the
