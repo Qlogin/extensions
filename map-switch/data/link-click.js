788cd942ed1dd195a263455fd142091a8252242a
Roman Kulagin <Roman.Kulagin@transas.com>
2015-11-18 19:02:25 +0300

    + mark current map service list item

--- map-switch/data/link-click.js
+++ map-switch/data/link-click.js
@@ -1,11 +1,12 @@
 // Get selection elements and setup callbacks
 function select(event) {
-   event.target.style.border = '2px solid blue';
+   event.target.style.border = '2px solid #b0da2e';
 }
 function unselect(event) {
    event.target.style.border = '2px solid white';
 }
 function open_link(event) {
+   unselect(event);
    self.port.emit('link-clicked', event.target.getAttribute('data-url'));
 }
 
@@ -20,6 +21,14 @@ for (i = 0; i != elems.length; ++i) {
 // Listen for the "show" event being sent from the
 // main add-on code. It means that the panel's about
 // to be shown.
-self.port.on("show", function onShow() {
-   // TODO:
+self.port.on('show', function onShow(url) {
+   var elems = document.getElementsByClassName('selection');
+   var i;
+   for (i = 0; i != elems.length; ++i) {
+      if (url.contains(elems[i].getAttribute('data-url'))) {
+         elems[i].children[0].style.display = 'inline';
+      } else {
+         elems[i].children[0].style.display = 'none';
+      }
+   }
 });
--- map-switch/index.js
+++ map-switch/index.js
@@ -1,23 +1,24 @@
 var buttons = require('sdk/ui/button/toggle');
-var panels = require("sdk/panel");
-var tabs = require("sdk/tabs");
-var self = require("sdk/self");
+var panels = require('sdk/panel');
+var tabs = require('sdk/tabs');
+var self = require('sdk/self');
 
 var panel = panels.Panel({
-   width : 204,
+   width : 224,
    height: 124,
-   contentURL: self.data.url("map-list.html"),
-   contentScriptFile: self.data.url("link-click.js"),
+   contentURL: self.data.url('map-list.html'),
+   contentScriptFile: self.data.url('link-click.js'),
+   onShow: handleShow,
    onHide: handleHide
 });
 
 var button = buttons.ToggleButton({
-   id: "map-list-button",
-   label: "Switch Map",
+   id: 'map-list-button',
+   label: 'Switch Map',
    icon: {
-      "16": "./icon-16.png",
-      "32": "./icon-32.png",
-      "64": "./icon-64.png"
+      '16': './icon-16.png',
+      '32': './icon-32.png',
+      '64': './icon-64.png'
    },
    onChange: handleChange
 });
@@ -30,11 +31,15 @@ function handleChange(state) {
    }
 }
 
+function handleShow() {
+   panel.port.emit('show', tabs.activeTab.url);
+}
+
 function handleHide() {
    button.state('window', {checked: false});
 }
 
-panel.port.on("link-clicked", function (url) {
+panel.port.on('link-clicked', function (url) {
    tabs.activeTab.url = url;
    panel.hide();
 });
