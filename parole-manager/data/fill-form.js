ab8fbdcaf1fc30c852fbf21691259b42d955ab18
Roman Kulagin <Roman.Qlogin@gmail.com>
2015-11-23 19:34:03 +0300

    parole-manager: update form filling

--- parole-manager/data/fill-form.js
+++ parole-manager/data/fill-form.js
@@ -1,10 +1,26 @@
-function on_fill_form(email, password) {
+function on_fill_form(username, password) {
    var pwd = $("input[type='password']").filter(":visible");
-   if (pwd.length == 1) {
-      pwd.val(password);
-      var user = $("input[name='username']").filter(":visible");
-      user.val(email);
-   }
+   if (pwd.length == 0)
+      return;
+
+   var form = pwd.get(0).form;
+   pwd.each(function(){
+       if (this.form != form)
+         form = null;
+   });
+   if (!form)
+      return;
+
+   pwd.val(password);
+
+   var selector = "input[name='username']";
+   if (username.contains('@'))
+      selector += ",input[name='email']";
+
+   var user = $(form).find(selector).filter(":visible");
+   user.each(function() {
+      $(this).val(username);
+   });
 }
 
 self.port.on("fill-form", on_fill_form);
