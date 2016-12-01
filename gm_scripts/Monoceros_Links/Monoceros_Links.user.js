965e4109c46cd3c73291ffddd23c5d78f8160da1
Roman Kulagin <Roman.Qlogin@gmail.com>
2016-12-01 14:03:56 +0300

    update monoceros user script

--- gm_scripts/Monoceros_Links/Monoceros_Links.user.js
+++ gm_scripts/Monoceros_Links/Monoceros_Links.user.js
@@ -3,7 +3,7 @@
 // @name         Monoceros Links
 // @namespace    Roman.Qlogin
 // @author       Roman Kulagin
-// @include      http://10.10.54.55:*
+// @include      http://10.10.74.11:*
 // @include      http://10.10.54.139:*
 // @description  Prettify DOM structure of Monoceros UI pages
 // @license      MIT
@@ -12,9 +12,66 @@
 // @downloadURL  https://qlogin.github.io/extensions/gm_scripts/Monoceros_Links/Monoceros_Links.user.js
 // @updateURL    https://qlogin.github.io/extensions/gm_scripts/Monoceros_Links/Monoceros_Links.user.js
 // @supportURL   https://github.com/qlogin/extensions/issues
-// @version      0.1
+// @version      0.2
 // @grant        none
 // ==/UserScript==
+
+var style_str = "" +
+"body { margin-top: 40px; }" +
+".navbar {" +
+"    font-family: monospace;" +
+"    position: fixed;" +
+"    top: 0;" +
+"    left: 0;" +
+"    background-color: lightgrey;" +
+"    padding: 10px;" +
+"    width: 100%;" +
+"    z-index: 100;" +
+"}" +
+".monoceros-logo {" +
+"    display: inline-block;" +
+"    background-image: url(\"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQsK3pvC3HzIRo9whCbUZBVAyDo-ZEetyDhdijefZQrh59VIWtK\");" +
+"    background-size: 100% 100%;" +
+"    width: 32px;" +
+"    height: 32px;" +
+"}" +
+".heading1 {" +
+"    padding-bottom: 10px;" +
+"    font-family: MV Boli;" +
+"}" +
+"select, input:not([type]) {" +
+"    padding: 3px 5px;" +
+"    margin: 2px 2px;" +
+"    display: inline-block;" +
+"    border: 1px solid #888;" +
+"    border-radius: 4px;" +
+"    box-sizing: border-box;" +
+"}" +
+".button-holder input[type=submit] {" +
+"    background-color: mediumseagreen;" +
+"	  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24);" +
+"    padding: 3px 5px;" +
+"    margin: 2px 2px;" +
+"    border-radius: 4px;" +
+"    cursor: pointer;" +
+"    color: white;" +
+"    text-shadow: 1px -1px 2px darkgreen;" +
+"}" +
+".button-holder input[type=submit]:hover { background-color: mediumaquamarine; }" +
+"#tasks_table td { vertical-align: top; }" +
+"#tasks_table tr:nth-child(even) { background-color: #f2f2f2; }" +
+"th { background-color: lightblue; }" +
+".form-header {" +
+"    background-color: lightblue;" +
+"    font-weight: bold;" +
+"    padding-top: 5px;" +
+"    padding-bottom: 5px;" +
+"    border: 1px solid #999;" +
+"}";
+
+var css = document.createElement("style");
+css.innerHTML = style_str;
+document.head.appendChild(css);
 
 var links = ["layers/ui", "layers", "tasks/ui", "tasks"];
 
@@ -34,7 +91,7 @@ for (i = 0; i != links.length; ++i) {
 }
 document.body.insertBefore(div, document.body.firstChild);
 
-var h1 = document.getElementsByTagName("h1")[0];
+var h1 = document.getElementsByClassName("heading1")[0];
 if (h1) {
    var logo = document.createElement("div");
    logo.setAttribute("class", "monoceros-logo");
@@ -48,124 +105,4 @@ if (h1) {
          }
       }
    }
-
-   var create_by_dir = document.getElementById("layer_from_dir_task_creation");
-   if (create_by_dir) {
-      forms = [];
-      for (i = 0; i != document.forms.length; ++i)
-         forms.push(document.forms.item(i));
-
-      table = document.createElement("table");
-      create_by_dir.parentNode.insertBefore(table, create_by_dir);
-      for (i = 0; i != forms.length; ++i) {
-         if (forms[i].id.startsWith("layer_frames_")) {
-            row = document.createElement("tr");
-            row.appendChild(document.createElement("td"));
-            row.appendChild(document.createElement("td"));
-            table.appendChild(row);
-            forms[i].children[1].style.width = "200px";
-            label = forms[i].children[0];
-            forms[i].removeChild(label);
-            row.children[0].appendChild(document.createTextNode(label.textContent));
-            row.children[0].style.padding = "5px";
-            row.children[1].appendChild(forms[i]);
-         }
-      }
-
-      wrapToFieldset(create_by_dir, "Create layer from directory:", 3);
-      var create_accurate = document.getElementById("accurate_layer_task_creation");
-      if (create_accurate) {
-           wrapToFieldset(create_accurate, "Create accurate layer:", 3);
-      }
-
-      var details = document.getElementsByTagName("details");
-      for (i = 0; i != details.length; ++i) {
-         var div = document.createElement("div");
-         div.style.display = "none";
-         div.appendChild(details[i].childNodes[1]);
-         details[i].appendChild(div);
-         var summary = details[i].childNodes[0];
-         summary.onclick = function(event) {
-              var next = event.target.nextSibling;
-              if (next.style.display === "none") {
-                 next.style.display = "block";
-              } else {
-                 next.style.display = "none";
-              }
-         };
-         //var iframe = document.createElement("iframe");
-         //iframe.src = "data:application/json;charset=utf-8," + encodeURI(details[i].childNodes[1].textContent);
-         //iframe.style.width = "100%";
-         //details[i].removeChild(details[i].childNodes[1]);
-         //details[i].appendChild(iframe);
-         //iframe.contentWindow.document.open("application/json", "replace");
-         //iframe.contentWindow.document.write(details[i].childNodes[1].textContent);
-         //iframe.contentWindow.document.close();
-      }
-   }
-
-   var h2 = document.getElementsByTagName('h2');
-   if (h2.length == 1 && h2[0].textContent == "Layers:") {
-      var layer = null;
-      var layers = document.getElementsByTagName('h3');
-      if (layers.length > 0)
-         layer = layers[0];
-
-      while (layer) {
-         var res = layer.textContent.match(/Layer '([a-zA-Z0-9_-]+)'/);
-         if (res) {
-            layer.innerHTML = "Layer '<a href=\"/layers/" + res[1] + "\">" + res[1] + "'</a>:";
-         }
-         var div = document.createElement("div");
-         div.setAttribute("class", "layer");
-         var child = layer.nextSibling;
-         layer.parentNode.insertBefore(div, child);
-         while (child && child.nodeName.toLowerCase() != "h3") {
-            var next = child.nextSibling;
-            if (child.nodeName.toLowerCase() == "table") {
-               var details = document.createElement("details");
-               var summary = document.createElement("summary");
-               summary.textContent = "[Click for information]";
-               summary.onclick = function(event) {
-                  var body = event.target.nextSibling;
-                  if (body.style.display == "none") {
-                     body.style.display = "table";
-                  } else {
-                     body.style.display = "none";
-                  }
-               }
-               details.appendChild(summary);
-               details.appendChild(child);
-               div.appendChild(details);
-               child.style.display = "none";
-               child = next;
-               continue;
-            } else if (child.nodeName.toLowerCase() == "form" &&
-                       child.getAttribute("action").endsWith('multi_angle')) {
-               child = wrapToFieldset(child, "Multi Angle", 0);
-            }
-            div.appendChild(child);
-            child = next;
-         }
-         layer = child;
-      }
-   }
-}
-
-function wrapToFieldset(element, fieldset_name, childnum) {
-   var parent = element.parentNode;
-   var div = document.createElement("p");
-   var fieldset = document.createElement("fieldset");
-   var title = document.createElement("legend");
-   title.appendChild(document.createTextNode(fieldset_name));
-   fieldset.appendChild(title);
-   div.appendChild(fieldset);
-   parent.insertBefore(div, element);
-
-   var index = Array.prototype.indexOf.call(parent.childNodes, element);
-   fieldset.appendChild(element);
-   while (--childnum >= 0) {
-      fieldset.appendChild(parent.childNodes[index]);
-   }
-   return div;
 }
